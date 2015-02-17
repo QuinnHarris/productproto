78738c94507b62e330522b4abd02b8dd1eaf719b
Quinn Harris <quinn@quinnharris.me>
2015-02-16 19:21:05 -0600

    Capistrano deploy

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 
 module Ink
-  DEFAULT_HOSTNAME = 'ornimo.com'
+  DEFAULT_HOSTNAME = 'zendecorator.com'
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
